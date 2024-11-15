8630707d60b0089e244b784fc46fb08f39463d96
StivenOsuna9909 <javierstivenosuna@hotmail.com>
2024-11-15 09:31:14 -0500

    commit

--- classes/server.ts
+++ classes/server.ts
@@ -5,9 +5,10 @@ import express from 'express';
 export default class Server {
 
     public app : express.Application;
-    public port: number = 3000;
+    public port: number;
 
     constructor() {
+        this.port = Number(process.env.PORT) || 3000;
         this.app = express();
     }
 
