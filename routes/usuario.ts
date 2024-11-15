a677a6a15812426be1d6be79636756f48794af08
StivenOsuna9909 <javierstivenosuna@hotmail.com>
2024-11-15 15:45:24 -0500

    corrigiendo

--- routes/usuario.ts
+++ routes/usuario.ts
@@ -13,7 +13,7 @@ userRoutes.post('/login', async (req: Request, res: Response) => {
 
     try {
         // Buscar al usuario por email
-        const userDB = await Usuario.findOne({ email: body.email }).exec();
+        const userDB = await Usuario.findOne({ email: body.email });
 
         // Si no se encuentra el usuario, retornamos un mensaje de error
         if (!userDB) {
@@ -53,6 +53,7 @@ userRoutes.post('/login', async (req: Request, res: Response) => {
         });
     }
 });
+
 
 
 
